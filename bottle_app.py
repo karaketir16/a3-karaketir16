c4f081f9ab62df566bc9f5c32a26214c375f871d
Osman <karaketir16@itu.edu.tr>
2017-12-19 14:31:43 +0300

    add background color choose

--- bottle_app.py
+++ bottle_app.py
@@ -6,12 +6,23 @@
 #####################################################################
 
 from bottle import route, run, default_app, debug, static_file, request
+from csv import reader
 
-def htmlify(title,text):
+contents = []
+input_file = open("a2_input.csv","r")
+for row in reader(input_file):
+	contents = contents + [row]
+
+def htmlify(bgcolor,title,text):
     page = """
         <!doctype html>
         <html lang="en">
             <head>
+                <style>
+                    table{
+                        background-color:%s;
+                    }
+                </style>
                 <meta charset="utf-8" />
                 <title>%s</title>
 		<link rel="stylesheet" href="static/style.css"/>
@@ -23,26 +34,62 @@ def htmlify(title,text):
             </body>
         </html>
 
-    """ % (title,text)
+    """ % (bgcolor,title,text)
     return page
 def staticFiles(filePath):
     return static_file(filePath,root='.')
 
 def index():
-    return htmlify("My lovely website",
-"""
+    options="""
 <form action="/show"method="POST">
-<input type="text" name="city"/>
-<input type="submit" value="test"/>
+<fieldset class="left">
+    <legend>Select Cities you want to see that's population</legend>
+<select name="city" size="10" multiple>;
+"""
+
+    a = 0
+    for rows in contents:
+        if a==0 or a>81:
+            a+=1
+            continue
+        opt="""<option value=%d>%s</option>""" % (a,rows[0])
+        options+=opt
+        a+=1
+    options+="""</select><br/>
+    You can choose multiple pressing ctrl</fieldset>
+    <fieldset class="left">
+    <legend>Select table background color</legend>
+    <input type="radio" name="bgcolor" value="white" checked/>white<br/>
+    <input type="radio" name="bgcolor" value="blue"/>blue<br/>
+    <input type="radio" name="bgcolor" value="green"/>green<br/>
+    <input type="radio" name="bgcolor" value="red"/>red<br/>
+    <input type="radio" name="bgcolor" value="yellow"/>yellow<br/>
+    <input type="radio" name="bgcolor" value="grey"/>grey<br/>
+    <input type="radio" name="bgcolor" value="pink"/>pink<br/>
+    <input type="radio" name="bgcolor" value="orange"/>orange<br/>
+    <input type="radio" name="bgcolor" value="black"/>black!!!!<br/>
+    </fieldset>
+    <br/>
+    <input type="checkbox" name="total" value="show"/>Show total population<br/>
+<input type="submit" value="test" />
 </form>
 """
-
-)
+    return htmlify("white","Populations",options)
 
 def show():
-    bbb=request.POST
-
-    return bbb['city']
+    cities=request.POST.getlist('city')
+    cont="""<table>
+    <tr><th>İl</th><th>2000</th><th>2001</th><th>2002</th><th>2003</th><th>2004</th><th>2005</th><th>2006</th><th>2007</th><th>2008</th><th>2009</th><th>2010</th></tr>
+    %s
+    </table>"""
+    cityRow=""
+    for city in cities:
+        cityRow+="<tr>"
+        for columns in contents[int(city)]:
+            cityRow+="<td>%s</td>"%columns
+        cityRow+"</tr>"
+    cont=cont%cityRow
+    return htmlify(request.POST.get("bgcolor"),"Populations",cont)
 
 route('/', 'GET', index)
 route('/static/<filePath>','GET', staticFiles)
